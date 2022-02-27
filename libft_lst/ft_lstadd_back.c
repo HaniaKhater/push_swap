bad087c830bcde879cd4438b02ccb52d4f37233e
Hania Khater <hania.khater2@hotmail.com>
2022-02-27 16:40:25 +0100

    libft list manipulations

---
+++ libft_lst/ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhater <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 03:09:08 by hkhater           #+#    #+#             */
+/*   Updated: 2021/12/12 03:09:29 by hkhater          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (*alst)
+	{
+		tmp = *alst;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->next = 0;
+	}
+	else
+		*alst = new;
+}
