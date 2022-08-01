ffccb5510b0f7c42f9a5cda83fe4cfc4f13e519c
HaniaKhater <khaterhania@gmail.com>
2022-08-01 05:53:34 +0300

    array utils 31/07

---
+++ array.c
@@ -0,0 +1,65 @@
+#include "push_swap.h"
+
+int     *fill_array(t_stack *s)
+{
+    int     i;
+    t_node  tmp;
+    int     *arr;
+
+    i = 0;
+    tmp = s->head;
+    arr = malloc(sizeof(int) * s->size + 1);
+    if (!tab)
+        return (NULL);
+    while (tmp->next)
+    {
+        arr[i] = tmp->nb;
+        i++;
+        tmp = tmp->next;
+    }
+    if (tmp)
+    {
+        arr[i] = tmp->nb;
+        i++;
+    }
+    arr[i] = '/0';
+    return (arr);
+    // error if empty?
+}
+
+int     array_len(int *arr)
+{
+    int     len;
+
+    len = 0;
+    while (arr[len])
+        len++;
+    return (len);
+}
+
+int     *sort_array(int *arr)
+{
+    int     i;
+    int     j;
+    int     len;
+    int     swap;
+
+    len = array_len(arr);
+    i = 0;
+    while (i < len)
+    {
+        j = i + 1;
+        while (j < n)
+        {
+            if (arr[i] > number[j])
+            {
+                swap = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swap;
+            }
+            j++;
+        }
+        i++;
+    }
+    return (arr);
+}
