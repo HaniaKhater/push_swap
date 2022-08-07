3ed7d2c5b683039cfb386d3281f0ec5037e22f2b
Hania hesham KHATER <hkhater@e1r11p21.clusters.42paris.fr>
2022-08-07 21:19:28 +0200

    tests

---
+++ test_fct.c
@@ -0,0 +1,32 @@
+#include "push_swap.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
+	char			c;
+
+	nb = n;
+	if (n < 0)
+	{
+		nb *= -1;
+		write(fd, "-", 1);
+	}
+	if (nb >= 10)
+		ft_putnbr_fd(nb / 10, fd);
+	c = (nb % 10) + 48;
+	write(fd, &c, 1);
+}
+
+void    print_stack(t_stack *s)
+{
+    t_node  *tmp;
+    tmp = s->head;
+    while (tmp->next)
+    {
+        ft_putnbr_fd(tmp->nb, 1);
+		write(1, " ", 1);
+        tmp = tmp->next;
+    }
+    ft_putnbr_fd(tmp->nb, 1);
+	write(1, " ", 1);
+}
